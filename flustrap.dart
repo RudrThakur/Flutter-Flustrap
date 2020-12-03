c854476e257d440eb257df6aa9420434057e44a6
Rudr Thakur <rudrakshacmkt777@gmail.com>
2020-12-03 21:03:40 +0530

    Initial commit

---
+++ flustrap.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+/*
+|__________________________________________
+|
+|             Backgrounds
+|__________________________________________
+*/
+
+const bg_primary = Color(0XFF8FCAF9);
+const bg_secondary = Color(0XFF443A9A);
+
+/*
+|__________________________________________
+|
+|             Fonts
+|__________________________________________
+*/
+
+// color
+const font_white = TextStyle(color: Colors.white);
+const font_black = TextStyle(color: Colors.black);
+
+// size
+const font_size_half = TextStyle(fontSize: 5);
+const font_size_1 = TextStyle(fontSize: 10);
+const font_size_2 = TextStyle(fontSize: 15);
+const font_size_3 = TextStyle(fontSize: 20);
+const font_size_4 = TextStyle(fontSize: 25);
+const font_size_5 = TextStyle(fontSize: 30);
+
+// weight
+const font_bold = TextStyle(fontWeight: FontWeight.bold);
+
+// weight_size
+const font_bold_size_half = TextStyle(fontWeight: FontWeight.bold, fontSize: 5);
+const font_bold_size_1 = TextStyle(fontWeight: FontWeight.bold, fontSize: 10);
+const font_bold_size_2 = TextStyle(fontWeight: FontWeight.bold, fontSize: 15);
+const font_bold_size_3 = TextStyle(fontWeight: FontWeight.bold, fontSize: 20);
+const font_bold_size_4 = TextStyle(fontWeight: FontWeight.bold, fontSize: 25);
+const font_bold_size_5 = TextStyle(fontWeight: FontWeight.bold, fontSize: 30);
+
+// color_weight
+const font_white_bold =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold);
+const font_black_bold =
+    TextStyle(color: Colors.black, fontWeight: FontWeight.bold);
+
+// color_weight_size
+const font_white_bold_size_half =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 5.0);
+const font_white_bold_size_1 =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 10.0);
+const font_white_bold_size_2 =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15.0);
+const font_white_bold_size_3 =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0);
+const font_white_bold_size_4 =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25.0);
+const font_white_bold_size_5 =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30.0);
+
+/*
+|__________________________________________
+|
+|             Padding
+|__________________________________________
+*/
+
+const pt_1 = const EdgeInsets.fromLTRB(0, 0, 10, 0);
+const pt_2 = const EdgeInsets.fromLTRB(0, 0, 20, 0);
+const pt_3 = const EdgeInsets.fromLTRB(0, 0, 30, 0);
+const pt_4 = const EdgeInsets.fromLTRB(0, 0, 40, 0);
+const pt_5 = const EdgeInsets.fromLTRB(0, 0, 50, 0);
+
+const pb_1 = const EdgeInsets.fromLTRB(0, 0, 0, 10);
+const pb_2 = const EdgeInsets.fromLTRB(0, 0, 0, 20);
+const pb_3 = const EdgeInsets.fromLTRB(0, 0, 0, 30);
+const pb_4 = const EdgeInsets.fromLTRB(0, 0, 0, 40);
+const pb_5 = const EdgeInsets.fromLTRB(0, 0, 0, 50);
+
+const pl_1 = const EdgeInsets.fromLTRB(10, 0, 0, 0);
+const pl_2 = const EdgeInsets.fromLTRB(20, 0, 0, 0);
+const pl_3 = const EdgeInsets.fromLTRB(30, 0, 0, 0);
+const pl_4 = const EdgeInsets.fromLTRB(40, 0, 0, 0);
+const pl_5 = const EdgeInsets.fromLTRB(50, 0, 0, 0);
+
+const pr_1 = const EdgeInsets.fromLTRB(0, 0, 10, 0);
+const pr_2 = const EdgeInsets.fromLTRB(0, 0, 20, 0);
+const pr_3 = const EdgeInsets.fromLTRB(0, 0, 30, 0);
+const pr_4 = const EdgeInsets.fromLTRB(0, 0, 40, 0);
+const pr_5 = const EdgeInsets.fromLTRB(0, 0, 50, 0);
+
+const p_half = const EdgeInsets.all(5);
+const p_1 = const EdgeInsets.all(10);
+const p_2 = const EdgeInsets.all(20);
+const p_3 = const EdgeInsets.all(30);
+const p_4 = const EdgeInsets.all(40);
+const p_5 = const EdgeInsets.all(50);
+
+const py_1 = const EdgeInsets.symmetric(vertical: 10);
+const py_2 = const EdgeInsets.symmetric(vertical: 20);
+const py_3 = const EdgeInsets.symmetric(vertical: 30);
+const py_4 = const EdgeInsets.symmetric(vertical: 40);
+const py_5 = const EdgeInsets.symmetric(vertical: 50);
+
+const px_1 = const EdgeInsets.symmetric(horizontal: 10);
+const px_2 = const EdgeInsets.symmetric(horizontal: 20);
+const px_3 = const EdgeInsets.symmetric(horizontal: 30);
+const px_4 = const EdgeInsets.symmetric(horizontal: 40);
+const px_5 = const EdgeInsets.symmetric(horizontal: 50);
